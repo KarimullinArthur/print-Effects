e85a3225657d504537a8efaa9fff512771317a06
Arthur <arthurkarimullin2005@gmail.com>
2021-12-14 21:10:58 +0500

    base2 clear

---
+++ main.py
@@ -0,0 +1,14 @@
+import random
+import time
+letters = ['h','e','l','o','w','r','d',' ']
+
+text = 'hello world'
+index = len(text)-1
+for i in text:
+    while True:
+        a = letters[random.randint(0,len(letters)-1)]
+        print(a,end='\r')
+        time.sleep(0.1)
+        if a == i:
+            print(i)
+            break   
