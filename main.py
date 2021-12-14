cbc579354781511cb8ae8eb8b53325df7b1ef31c
Arthur <arthurkarimullin2005@gmail.com>
2021-12-14 21:40:43 +0500

    Справились с экранизацией осталось сделать движение самого пербора

--- main.py
+++ main.py
@@ -4,11 +4,15 @@ letters = ['h','e','l','o','w','r','d',' ']
 
 text = 'hello world'
 index = len(text)-1
+b = index-1
+s = " "
 for i in text:
     while True:
         a = letters[random.randint(0,len(letters)-1)]
         print(a,end='\r')
         time.sleep(0.1)
         if a == i:
-            print(i)
+            print(text[:index-b],end='')
+            b -= 1
+            s += " "
             break   
