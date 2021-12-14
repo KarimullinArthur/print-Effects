78875310fe8269c3792979bc023f0648434f67be
Arthur <arthurkarimullin2005@gmail.com>
2021-12-14 22:26:31 +0500

    Почти готово, но...

--- main.py
+++ main.py
@@ -4,15 +4,14 @@ letters = ['h','e','l','o','w','r','d',' ']
 
 text = 'hello world'
 index = len(text)-1
-b = index-1
-s = " "
+t = 1
 for i in text:
     while True:
         a = letters[random.randint(0,len(letters)-1)]
-        print(a,end='\r')
+        print(text[:t],a,sep='',end='\r')
         time.sleep(0.1)
         if a == i:
-            print(text[:index-b],end='')
-            b -= 1
-            s += " "
-            break   
+            t += t
+            break
+
+print(text)
