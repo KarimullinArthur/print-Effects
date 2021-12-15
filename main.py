e353e2b46224bb434edeaf233ec184c66f5298bf
Arthur <arthurkarimullin2005@gmail.com>
2021-12-15 15:34:00 +0500

    OK it work

--- main.py
+++ main.py
@@ -6,14 +6,14 @@ index = len(text)-1
 t = 1
 for i in text:
     while True:
-        a = chr(random.randint(65,122))
+        a = chr(random.randint(32,126))
         if t == 1:
             print(a,end='\r')
         elif t == index:
             break
         else:
             print(text[:t],a,sep='',end='\r')
-        time.sleep(0.1)
+        time.sleep(0.05)
         if a == i:
             t += 1
             break
